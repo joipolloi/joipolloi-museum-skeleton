0b64742653484ff998907ba7accc589194689fc9
Josh <josh.kalsi@joipolloi.com>
2022-04-11 09:22:48 +0100

    Removing NLM specific types

--- theme-src/joipolloi/inc/allowedBlocks.php
+++ theme-src/joipolloi/inc/allowedBlocks.php
@@ -4,33 +4,30 @@ namespace Joi\AllowedBlocks;
 
 add_action('allowed_block_types_all', function ($allowed_block_types, $block_editor_context) {
 
-    if (!$block_editor_context->post) {
-        return $allowed_block_types;
-    }
-
-    if ($block_editor_context->post->post_type == 'event') {
-         return array(
-        'core/image',
-        'core/html',
-        'core/quote',
-        'core/embed',
-        'core/youtube',
-        'core/paragraph',
-        'core/heading',
-        'core/list',
-        'core/group',
-        'core/buttons',
-        'core/shortcode',
-        'core-embed/youtube',
-        'core/columns',
-        'acf/accordion',
-        'acf/image-carousel',
-        'acf/image-colour',
-        'acf/pullout',
-        'acf/virtual-tour',
-        'acf/download'
-        );
-    }
+    // EXAMPLE - LIMIT FOR SPECIFIC POST TPYE
+    // if ($block_editor_context->post->post_type == 'event') {
+    //      return array(
+    //     'core/image',
+    //     'core/html',
+    //     'core/quote',
+    //     'core/embed',
+    //     'core/youtube',
+    //     'core/paragraph',
+    //     'core/heading',
+    //     'core/list',
+    //     'core/group',
+    //     'core/buttons',
+    //     'core/shortcode',
+    //     'core-embed/youtube',
+    //     'core/columns',
+    //     'acf/accordion',
+    //     'acf/image-carousel',
+    //     'acf/image-colour',
+    //     'acf/pullout',
+    //     'acf/virtual-tour',
+    //     'acf/download'
+    //     );
+    // }
 
     return array(
         'core/image',
--- theme-src/joipolloi/inc/search.php
+++ theme-src/joipolloi/inc/search.php
@@ -4,7 +4,7 @@ namespace Joi\Search;
 
 add_filter('pre_get_posts', function ($query) {
     if ($query->is_main_query() && !is_admin() && $query->is_search) {
-        $query->set('post_type', array('event', 'collection', 'service', 'discover'));
+        $query->set('post_type', array('post'));
     }
     return $query;
 });
